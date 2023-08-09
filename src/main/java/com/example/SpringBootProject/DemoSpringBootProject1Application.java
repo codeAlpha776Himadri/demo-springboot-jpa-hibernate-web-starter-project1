4b92495f57ef3509377b69b55e585e8719e5e0ea
codeAlpha776Himadri <hck364864@gmail.com>
2023-08-09 21:53:37 +0530

    regular

---
+++ src/main/java/com/example/SpringBootProject/DemoSpringBootProject1Application.java
@@ -0,0 +1,36 @@
+package com.example.SpringBootProject;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.SpringBootProject.dao.UserDao;
+import com.example.SpringBootProject.model.User;
+
+@SpringBootApplication
+public class DemoSpringBootProject1Application implements CommandLineRunner {
+    
+    @Autowired
+    UserDao userDao ;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootProject1Application.class, args) ;
+    }
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+
+        List<User> users = userDao.findAll() ;
+        for (User u : users) {
+            System.out.println(u);
+        }
+        
+    }
+
+}
