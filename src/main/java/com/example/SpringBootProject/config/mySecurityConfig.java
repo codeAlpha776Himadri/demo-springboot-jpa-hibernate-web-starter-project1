ab7e75abd454b0971901f37937f86ad01a4cb444
codeAlpha776Himadri <hck364864@gmail.com>
2023-08-13 00:12:46 +0530

    regular

--- src/main/java/com/example/SpringBootProject/config/mySecurityConfig.java
+++ src/main/java/com/example/SpringBootProject/config/mySecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.SpringBootProject.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity ;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)  // enables method security 
 public class mySecurityConfig {
     
 
--- src/main/java/com/example/SpringBootProject/controller/UserController.java
+++ src/main/java/com/example/SpringBootProject/controller/UserController.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ public class UserController {
     }
 
     
+    @PreAuthorize("hasRole('USER')")
     @PostMapping(value = "/add")
     /*  @RequestBody - used to get body parameters from the request */
     public ResponseEntity<Object> addUser(@RequestBody SecuredUser user) {
